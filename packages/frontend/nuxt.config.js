0517ed4edc77aba3d2304980a43af7eb338df7a5
Atsukoro1 <dornicakkuba@gmail.com>
2022-09-01 19:56:02 +0200

    Unblotat

--- packages/frontend/nuxt.config.js
+++ packages/frontend/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: 'Moshiro',
     htmlAttrs: {
       lang: 'en'
     },
@@ -15,33 +15,22 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  server: {
+    host: '127.0.0.1',
+    port: 5500
+  },
   css: [
   ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/chakra
     '@chakra-ui/nuxt',
-    // https://go.nuxtjs.dev/emotion
     '@nuxtjs/emotion',
   ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
 }
--- packages/server/README.md
+++ packages/server/README.md
@@ -10,7 +10,7 @@
     <img src="https://img.shields.io/badge/license-MIT-blue.svg">
 </div>
 
-# Frontend
+# Backend
 - WIP
 
 ## Contributing
